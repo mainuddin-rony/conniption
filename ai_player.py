3d5edc1f720d8eceddc78aba5175bd1206a58658
Md Main Uddin Rony <ronybuetcse119@gmail.com>
2018-05-08 19:22:25 -0500

    Update ai_player.py

--- ai_player.py
+++ ai_player.py
@@ -8,25 +8,25 @@ class AIPlayer():
     depth = None
     flip = None
 
-    def __init__(self, name, color, flip, difficulty=3):
+    def __init__(self, name, disk, flip, difficulty=3):
         self.type = "AI"
         self.name = name
-        self.color = color
+        self.disk = disk
         self.depth = difficulty
         self.flip = flip
 
     def move(self, state, curr_player_flip, opp_player_flip, last_move):
-        print("{0}'s turn.  {0} is {1}".format(self.name, self.color))
+        print("{0}'s turn.  {0} is {1}".format(self.name, self.disk))
 
         # Deciding who is the opponent player npw
-        if self.color == 'x':
+        if self.dsik == 'x':
             opp_player = 'o'
         else:
             opp_player = 'x'
 
         m = Minimax(state)
         #creating a game board instance
-        gameBoard = GameBoard(self.color, opp_player, curr_player_flip, opp_player_flip, last_move, state)
+        gameBoard = GameBoard(self.disk, opp_player, curr_player_flip, opp_player_flip, last_move, state)
         best_move, value, move_type = m.bestMove(self.depth, gameBoard)
         return best_move, move_type
 
